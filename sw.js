408325d3542b111238f3c4cbde497c6025e04d39
kokoyori-sekainiitamio <a123asd123asd1@gmail.com>
2025-06-29 15:20:00 +0800

    Site updated: 2025-06-29 15:20:00

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1751174872982";const preCache = ["/images/taichi.png","/images/600211.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751181599599";const preCache = ["/images/taichi.png","/images/600211.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
